c80974edf509511a5b3aebf33eacc6f6c38014d2
= <luis@Luiss-MacBook-Pro.local>
2022-01-31 11:40:46 -0500

    up info

--- javascript/main.js
+++ javascript/main.js
@@ -1,53 +1,53 @@
 window.addEventListener('scroll', function() {
     let animado = document.getElementById('animado');
-    let posicion = animado.getBoundingClientRect().top;
+    let posicion = animado.offsetTop;
     console.log(posicion);
-    let pantalla = window.innerHeight/1;
+    let pantalla = window.scrollY;
 
-    if(posicion < pantalla) {
-        animado.style.animation = 'mover 2s ease-out'
+    if(posicion - 200 < pantalla) {
+        animado.classList.add('appear')
     }
 })
 window.addEventListener('scroll', function() {
     let animar = document.getElementById('animar');
-    let posicion = animar.getBoundingClientRect().top;
+    let posicion = animar.offsetTop;
     console.log(posicion);
-    let pantalla = window.innerHeight/1;
+    let pantalla = window.scrollY;
 
-    if(posicion < pantalla) {
-        animar.style.animation = 'mover 3s ease-out forwards' 
+    if(posicion - 200 < pantalla) {
+        animar.classList.add('appear') 
     }
 })
 
 window.addEventListener('scroll', function() {
     let mover = document.getElementById('mover');
-    let posicion = mover.getBoundingClientRect().top;
+    let posicion = mover.offsetTop;
     console.log(posicion);
-    let pantalla = window.innerHeight/1;
+    let pantalla = window.scrollY;
 
-    if(posicion < pantalla) {
-        mover.style.animation = 'mover 4s ease-out forwards' 
+    if(posicion - 200 < pantalla) {
+        mover.classList.add('appear') 
     }
 })
 
 window.addEventListener('scroll', function() {
     let subir = document.getElementById('subir');
-    let posicion = subir.getBoundingClientRect().top;
+    let posicion = subir.offsetTop;
     console.log(posicion);
-    let pantalla = window.innerHeight/1;
+    let pantalla = window.scrollY;
 
-    if(posicion < pantalla) {
-        subir.style.animation = 'mover 4s ease-out forwards' 
+    if(posicion - 200 < pantalla) {
+        subir.classList.add('appear') 
     }
 })
 
 window.addEventListener('scroll', function() {
     let move = document.getElementById('move');
-    let posicion = move.getBoundingClientRect().top;
+    let posicion = move.offsetTop;
     console.log(posicion);
-    let pantalla = window.innerHeight/1;
+    let pantalla = window.scrollY;
 
-    if(posicion < pantalla) {
-        move.style.animation = 'mover 4s ease-out forwards' 
+    if(posicion - 200 < pantalla) {
+        move.classList.add('appear') 
     }
 })
